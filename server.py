93e5d389dc93701945ea3e4ff39588c0ab931b99
Sze Yan <89469273+szeyan543@users.noreply.github.com>
2024-06-06 10:23:26 -0400

    add header

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template_string #allow html templates rendering from string
+from flask import Flask, render_template_string, make_response
 import requests
 
 WEB_SERVER_BIND_ADDRESS = '0.0.0.0'
 WEB_SERVER_PORT = 8000
 
-LOCAL_AGENT_URL = 'http://localhost:8510/service'  
+LOCAL_AGENT_URL = 'http://localhost:8510/service'
 
 app = Flask('node-ui')
 
@@ -26,16 +26,19 @@ template = """
 def get_services_status():
     try:
         response = requests.get(LOCAL_AGENT_URL)
-        response.raise_for_status() #raise an HTTPError for 4xx or 5xx
+        response.raise_for_status()  # raise an HTTPError for 4xx or 5xx
         services = response.json()  # Assuming the response is in JSON format
         return services
-    except requests.RequestException as e: #network error, server down, non-2xx status code
+    except requests.RequestException as e:  # network error, server down, non-2xx status code
         return {"error": str(e)}
 
 @app.route("/", methods=['GET'])
 def index():
     status = get_services_status()
-    return render_template_string(template, services=status)
+    response = make_response(render_template_string(template, services=status))
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 if __name__ == '__main__':
     app.run(host=WEB_SERVER_BIND_ADDRESS, port=WEB_SERVER_PORT)
